6b6b41d6a46d9f6bc25f010e623086a8bfb959db
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-03-19 09:19:29 +0100

    test() Functional tests
    Tests with ptython cucumber
    
    S10,L109

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -1,20 +1,18 @@
 from behave import *
 from selenium import webdriver
+from unittest import TestCase
 
 use_step_matcher('re')
+tc = TestCase('__init__')
+
 
 @given('I am on the home page')
 def step_impl(context):
-    browser = webdriver.Chrome()
-    browser.get('http://127.0.0.1:5002/')
+    context.browser = webdriver.Chrome()
+    context.browser.get('http://127.0.0.1:5002/')
 
 
-
-# Feature: Test navigation between pages
-#   We can have a longer description that
-#   can span a few lines
-#
-#   Scenario: Homepage can go to Blog
-#     Given I am on the home page
-#     When I clock on the link with id "blog-link"
-#     Then I am on the blog page
+@then('I am on the blog page')
+def step_impl(context):
+    expected_url = 'http://127.0.0.1:5002/blog'
+    tc.assertEqual(expected_url, context.browser.current_url)
