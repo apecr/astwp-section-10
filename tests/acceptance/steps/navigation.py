a2e8d4649d001937294a0bde5453b0466006c998
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-03-19 09:27:13 +0100

    test() New scenario
    Include new scenario
    
    S10,L110

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -16,3 +16,15 @@ def step_impl(context):
 def step_impl(context):
     expected_url = 'http://127.0.0.1:5002/blog'
     tc.assertEqual(expected_url, context.browser.current_url)
+
+
+@given("I am on the blog page")
+def step_impl(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get('http://127.0.0.1:5002/blog')
+
+
+@then("I am on the home page")
+def step_impl(context):
+    expected_url = 'http://127.0.0.1:5002/'
+    tc.assertEqual(expected_url, context.browser.current_url)
